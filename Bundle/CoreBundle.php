b03b2d932e8808673e20f09b845c60a95aa6a7df
Alexey Gromov <italianwind@gmail.com>
2014-02-14 14:14:54 +0400

    NEW Added CoreBundle interface

---
+++ Bundle/CoreBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace NS\CoreBundle\Bundle;
+
+/**
+ * Interface CoreBundle
+ *
+ * @package NS\CoreBundle\Bundle
+ */
+interface CoreBundle
+{
+    /**
+     * Retrieves human-readable bundle title
+     * @return string
+     */
+    public function getTitle();
+} 
