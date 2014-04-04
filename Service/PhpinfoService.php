14de08034ffa8b8597e519bb255ef83b3a1d25dc
Alexey Gromov <italianwind@gmail.com>
2014-04-04 21:44:07 +0400

    NEW Added php information service

---
+++ Service/PhpinfoService.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace NS\CoreBundle\Service;
+
+/**
+ * Class PhpInfoService
+ *
+ * @package NS\CoreBundle\Service
+ */
+class PhpinfoService
+{
+    /**
+     * @return array
+     */
+    public function getInfo()
+    {
+        $user  = posix_getpwuid(posix_geteuid());
+        $group = posix_getgrgid($user['gid']);
+
+        $info = array(
+            // general
+            'general' => array(
+                'version' => phpversion(),
+                'user'    => $user,
+                'group'   => $group,
+            ),
+
+            // php.ini info
+            'phpIni' => array(
+                'loaded'  => php_ini_loaded_file(),
+                'scanned' => php_ini_scanned_files(),
+            ),
+
+            // file upload info
+            'fileUpload' => $this->iniValues(array(
+                'file_uploads',
+                'upload_max_filesize',
+                'post_max_size',
+                'max_input_time',
+                'upload_tmp_dir',
+                'max_file_uploads',
+            )),
+        );
+        return $info;
+    }
+
+    private function iniValues(array $names)
+    {
+        $values = array();
+        foreach ($names as $name) {
+            $values[$name] = (ini_get($name)?:'n/a') . ' (' . (get_cfg_var($name)?:'n/a') . ')';
+        }
+        return $values;
+    }
+} 
